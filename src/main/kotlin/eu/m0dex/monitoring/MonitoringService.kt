f9a504ae968fd523f822a3eae6a336d8c577a2fa
Jakub Kubík <jakub.kubik.it@protonmail.com>
2023-05-17 20:18:33 +0200

    refactor(Monitor): refactor code to enable future REST API to see monitored services

--- src/main/kotlin/eu/m0dex/monitoring/MonitoringService.kt
+++ src/main/kotlin/eu/m0dex/monitoring/MonitoringService.kt
@@ -1,10 +1,16 @@
 package eu.m0dex.monitoring
 
 import eu.m0dex.monitoring.monitor.Monitor
+import eu.m0dex.monitoring.monitor.MonitoredService
+import eu.m0dex.monitoring.monitor.QuestDBWriter
+import eu.m0dex.monitoring.monitor.StatusMessage
 import eu.m0dex.monitoring.service.ILoggable
 import eu.m0dex.monitoring.service.IService
 import io.questdb.client.Sender
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.joinAll
+import kotlinx.coroutines.launch
 import org.jetbrains.exposed.sql.Database
 
 class MonitoringService(
@@ -12,11 +18,33 @@ class MonitoringService(
     private val questDbSender: Sender,
     private val questDbReader: Database,
 ) : IService, ILoggable {
+
+    private val questDbChannel = Channel<StatusMessage>(Channel.UNLIMITED)
+
+    private val monitoredServices = config.monitored.map { (serviceName, serviceConfig) ->
+        MonitoredService(
+            questDbChannel = questDbChannel,
+            name = serviceName,
+            displayName = serviceConfig.displayName,
+            url = serviceConfig.url,
+            method = serviceConfig.method,
+            payload = serviceConfig.payload,
+            additionalHeaders = serviceConfig.additionalHeaders,
+            expectedStatusCodes = serviceConfig.expectedStatusCodes,
+            expectedResponseRegex = serviceConfig.expectedResponseRegex,
+            intervalMillis = serviceConfig.intervalMillis,
+            timeoutMillis = serviceConfig.timeoutMillis,
+        )
+    }.toSet()
+
     override suspend fun run() = coroutineScope {
-        Monitor(
-            questDbSender,
-            config.monitored,
-        ).run()
+        val questDbWriter = QuestDBWriter(questDbSender, questDbChannel)
+        val monitor = Monitor(monitoredServices)
+
+        joinAll(
+            launch { questDbWriter.run() },
+            launch { monitor.run() }
+        )
 
         // TODO: Add a REST API provider and a web UI
     }
--- src/main/kotlin/eu/m0dex/monitoring/monitor/Monitor.kt
+++ src/main/kotlin/eu/m0dex/monitoring/monitor/Monitor.kt
@@ -1,43 +1,19 @@
 package eu.m0dex.monitoring.monitor
 
-import eu.m0dex.monitoring.MonitoredServiceConfig
 import eu.m0dex.monitoring.service.ILoggable
-import io.questdb.client.Sender
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.joinAll
 import kotlinx.coroutines.launch
 
 class Monitor(
-    private val questDbSender: Sender,
-    serviceConfigs: Map<String, MonitoredServiceConfig>,
+    private val monitoredServices: Set<MonitoredService>,
 ) : ILoggable {
 
-    private val questDbChannel = Channel<StatusMessage>()
-
-    private val monitoredServices: Set<MonitoredService> =
-        serviceConfigs.map { (serviceName, serviceConfig) ->
-            MonitoredService(
-                questDbChannel = questDbChannel,
-                name = serviceName,
-                displayName = serviceConfig.displayName,
-                url = serviceConfig.url,
-                method = serviceConfig.method,
-                expectedResponseRegex = serviceConfig.expectedResponseRegex,
-                additionalHeaders = serviceConfig.additionalHeaders,
-                intervalMillis = serviceConfig.intervalMillis,
-            )
-        }.toSet()
-
     suspend fun run() = coroutineScope {
-        val questDbWriter = QuestDBWriter(questDbSender, questDbChannel)
+        logger.info("Starting monitoring of ${monitoredServices.size} services")
 
-        val jobs = monitoredServices
+        monitoredServices
             .map { launch { it.checkAvailability() }}
-            .toMutableList()
-
-        jobs.add(launch { questDbWriter.run() })
-
-        jobs.joinAll()
+            .joinAll()
     }
 }
