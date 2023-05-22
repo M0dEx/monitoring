b99dcc8dac23ca6460f5c7414d6e80e18c7f14a4
Jakub Kubík <jakub.kubik.it@protonmail.com>
2023-05-22 18:27:48 +0200

    fix(QuestDBWriter): use nanoseconds instead of milliseconds

--- src/main/kotlin/eu/m0dex/monitoring/monitor/QuestDBWriter.kt
+++ src/main/kotlin/eu/m0dex/monitoring/monitor/QuestDBWriter.kt
@@ -5,12 +5,17 @@ import io.questdb.client.Sender
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.datetime.Clock
+import kotlinx.datetime.Instant
 
 @OptIn(DelicateCoroutinesApi::class)
 class QuestDBWriter(
     private val questDbSender: Sender,
     private val questDbChannel: Channel<Pair<String, Status>>
 ) {
+    companion object {
+        private fun Instant.toEpochNanoseconds(): Long = this.toEpochMilliseconds() * 1_000_000 + this.nanosecondsOfSecond
+    }
+
     suspend fun run() = withContext(Dispatchers.IO) {
         while (isActive && !questDbChannel.isClosedForReceive) {
             val (name, status) = questDbChannel.receive()
@@ -23,7 +28,7 @@ class QuestDBWriter(
             status.latency?.let { row.longColumn("latency", it) }
             status.failReason?.let { row.stringColumn("failReason", it) }
 
-            status.timestamp?.let { row.at(it.epochSeconds) } ?: row.at(Clock.System.now().toEpochMilliseconds())
+            status.timestamp?.let { row.at(it.epochSeconds) } ?: row.at(Clock.System.now().toEpochNanoseconds())
         }
     }
 }
